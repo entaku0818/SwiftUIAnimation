09f4f95a82e7c8e85ea2f003b522cf58e1706bef
entaku <entaku19890818@gmail.com>
2025-03-09 21:09:20 +0900

    first commit

---
+++ SwiftUIAnimation/SwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationApp.swift
+//  SwiftUIAnimation
+//
+//  Created by 遠藤拓弥 on 2025/03/09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
